e676c48118e739188934a74daafb8476a46af425
jafermjj <jafermjj@gmail.com>
2022-10-01 22:03:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 
 streamlit.title('My parents new healthy diner')
-streamlit.header('🥣Breakfast Menu')
-
+streamlit.header('Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔Kale, spinach & rocket Smoothie')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
